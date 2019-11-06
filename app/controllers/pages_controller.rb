94d75309ddc52d6fc2d31cf2a6de4aa849a6d6bc
Doug Watson <dougwatson@outlook.com>
2019-11-05 19:48:43 -0900

    initial xml working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,14 @@ class PagesController < ApplicationController
     @xml = '<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0">' +
     '<channel><title>FNBA Headlines</title>' +
     '<link>http://localhost:3000/</link><description>First National Bank News and Updates</description>'
-    document = Nokogiri::HTML.parse(open('https://www.fnbalaska.com'))
-    document.css("div.content-well.current-headlines li").each do |list|
+    document = Nokogiri::HTML.parse(open('https://www.fnbalaska.com/about-us/about-first-national/press-releases'))
+    document.css("div#PressBoss td").each do |list|
       @xml += "<item><title>#{list.content}</title><link>#{list[:href]}</link><description>Read More.</description></item>"
     end
     @xml += '</channel></rss>'
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @xml }
+    end
   end
 end
