0f6fe6aef7403740f900d19577ce5fa736f73c9b
Doug Watson <dougwatson@outlook.com>
2019-11-05 20:59:08 -0900

    Working Manually typed xml

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,16 @@ class PagesController < ApplicationController
     require 'open-uri'
     require 'nokogiri'
     @xml = '<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0">' +
-    '<channel><title>FNBA Headlines</title>' +
-    '<link>http://localhost:3000/</link><description>First National Bank News and Updates</description>'
+            '<channel><title>FNBA Headlines</title>' +
+            '<link>https://rss-fnba-feed.herokuapp.com/pages.xml</link>' + 
+            '<description>First National Bank Alaska News and Updates</description>'
     document = Nokogiri::HTML.parse(open('https://www.fnbalaska.com/about-us/about-first-national/press-releases'))
-    document.css("div#PressBoss td").each do |list|
-      @xml += "<item><title>#{list.content}</title><link>#{list[:href]}</link><description>Read More.</description></item>"
+    document.css("div#PressBoss tr").each_with_index do |row,index|
+      unless index == 0
+        @xml += "<item><title>#{row.children[1].text}</title><link>" + 
+                "#{row.children[3].children.first[:href]}</link><description>" +
+                "#{row.children[3].content.gsub(/&/,'')}</description></item>"
+      end
     end
     @xml += '</channel></rss>'
     respond_to do |format|
